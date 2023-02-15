fa91a74144b17c2548a6275db830bb19e9192170
Ameriolate <123063879+Ameriolate@users.noreply.github.com>
2023-02-15 11:14:08 +0300

    Update guessing game.py

--- guessing game.py
+++ guessing game.py
@@ -6,7 +6,6 @@ if guess==winning_number:
     print ("you WIN!")
 elif guess > winning_number:
     print ("too high")
-    guess = int(input("Enter an integer: "))
 else:
     print ("too low!")
     
