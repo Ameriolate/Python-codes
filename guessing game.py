73fcc172c373ec53436051b5b1eab0af95f81c11
Ameriolate <123063879+Ameriolate@users.noreply.github.com>
2023-02-15 11:15:42 +0300

    Update guessing game.py

--- guessing game.py
+++ guessing game.py
@@ -1,7 +1,6 @@
 import random
 winning_number = random.randint(5,5 )
 guess = int(input("Enter an integer: "))
-#while True:
 if guess==winning_number:
     print ("you WIN!")
 elif guess > winning_number:
