2689c6c601c83fe7e6aa838501580ce1306c0f1a
Ameriolate <123063879+Ameriolate@users.noreply.github.com>
2023-03-12 20:22:22 +0300

    Create multiple inheritance.py

---
+++ multiple inheritance.py
@@ -0,0 +1,13 @@
+class Legs:
+ def number(self):
+  print ("It has four legs")
+class Fur:
+ def side(self):
+  print("It's fur is short")
+class Cow(Legs,Fur):
+ def mow(self):
+  print("It's a cow")
+c.cow() 
+c.number()
+c.size()
+c.mow()
